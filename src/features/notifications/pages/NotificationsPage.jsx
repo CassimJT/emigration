065334893a6f174516ad96035dc5a4ee0fdbff98
Light bekete <bsc-com-ne-07-21@unima.ac.mw>
2025-12-31 17:48:54 +0200

    pulled dev01 changes

--- src/features/notifications/pages/NotificationsPage.jsx
+++ src/features/notifications/pages/NotificationsPage.jsx
@@ -13,7 +13,7 @@ export function NotificationsPage() {
     <div className="container mx-auto max-w-4xl px-4 py-6 sm:py-8 md:py-10">
 
       
-      <NotificationBell />
+      {/* <NotificationBell /> */}
       <NotificationsHeader />
 
       
