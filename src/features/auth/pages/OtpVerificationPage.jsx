7b3dc751b379ebadfa56890dc5c062bf8cea7d94
bryan-nathupo <bryannathupo@gmail.com>
2026-01-01 13:50:37 +0200

    OtpVerificationPage implemented

--- src/features/auth/pages/OtpVerificationPage.jsx
+++ src/features/auth/pages/OtpVerificationPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import OtpForm from '../components/OtpForm'
 
 function OtpVerificationPage() {
   return (
-    <div>OtpVerificationPage</div>
+    <div className="items-center justify-center bg-white  ">
+        <OtpForm />
+    </div>
   )
 }
 
