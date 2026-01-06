67d41e9fde22db00bc24ce607fb2141278d0fdb7
bryan-nathupo <bryannathupo@gmail.com>
2026-01-06 13:44:50 +0200

    LoginPage re-implemented

--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -1,16 +1,62 @@
 import React from "react"
+import { cn } from "@/lib/utils"
 import Logo from "@/assets/Logo.svg"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import {Card, CardContent} from "@/components/ui/card"
+import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+
 //import { LoginForm }  from '@/components/login-form'
-import { LoginForm as ShadcnLoginForm } from '@/components/login-form'
 
+export default function LoginForm({ phoneNumber, onChange, onSubmit, loading , className, ...props }) {
 
-export default function LoginForm({ phoneNumber, onChange, onSubmit, loading }) {
+  const navigate = useNavigate()
+  const moveToOtpPage = ()=> {
+    navigate("/otp")
+  }
+
   return (
-    <ShadcnLoginForm />
+    
+        <form className={cn("flex flex-col gap-6 p-6 md:p-8 pb-12 bg-gray-200 rounded-xl", className)} {...props}>
+          
+            <div className="flex flex-col items-center gap-1 text-center">
+             <img
+              src={Logo}
+              alt="coart of arm logo"
+              className="opacity-50 w-40 h-40 mx-auto"
+            />
+              <h1 className="text-lg font-semibold">Log In</h1>
+            </div>
+            
+            <div className="grid gap-4 mt-6 items-center justify-center">
+              <Label htmlFor="Username" className="font-bold text-lg">Username</Label>
+                <Input className="rounded-xl border-opacity-30 border-black h-14 w-80 placeholder:text-lg placeholder:text-gray-500 text-lg"
+                  id="nationalId" type="nationalId" 
+                  placeholder="Enter username" 
+                  required />
 
+              <Label htmlFor="Username" className="font-bold text-lg">Password</Label>
+                <Input className="rounded-xl border-opacity-30 border-black h-14 w-80 placeholder:text-lg placeholder:text-gray-500 text-lg"
+                  id="nationalId" type="nationalId" 
+                  placeholder="Enter password" 
+                  required />
+             </div>
+              <div className="flex flex-row items-center justify-center gap-8 mt-6 mb-20">
+                <Button 
+                  onClick={moveToOtpPage}
+                  type="submit"
+                  className="h-16 w-40 rounded-full bg-yellow-500 text-black hover:bg-yellow-600 text-xl font-bold" >
+                  Login
+                </Button>
+                 <Link 
+                  to="/signup"
+                  className=" text-sm font-semibold text-blue-600 hover:underline">
+                  Create account
+                 </Link>
+                {/* link to be implemented */}
+              </div> 
+        </form>
  )
 }       
--- src/features/auth/components/OtpForm.jsx
+++ src/features/auth/components/OtpForm.jsx
@@ -20,95 +20,124 @@ export default function LoginForm({
 
 }) {
   const navigate = useNavigate()
-  const moveToLoginPage = ()=> {
+  const moveToOtpPage = ()=> {
     navigate("/login")
   }  
   return (
    
-    <div className={cn("flex flex-col gap-6 h-screen ", className)} {...props}>
-      <Card className="h-screen w-screen">
-        <CardContent className="grid p-0 md:grid-cols-2 h-0">
-          <form className="p-6 md:p-8 pb-12 bg-gray-300">
-            <div className="flex flex-col gap-6 pb-20 justify-between">
-              <div className="flex flex-col gap-8 items-center text-center ">
-                 <img
-                   src={Logo}
-                   alt="coart of arm logo"
-                   className="opacity-50 w-32 h-40 mx-auto"
-                />
-                <h1 className="text-lg font-bold">Lets verify who you are</h1>
-                <p>Enter the OTP received via your email</p>
-                <div className="flex items-center">
+  //   <div className={cn("flex flex-col gap-6 h-screen ", className)} {...props}>
+  //     <Card className="h-screen w-screen">
+  //       <CardContent className="grid p-0 md:grid-cols-2 h-0">
+  //         <form className="p-6 md:p-8 pb-12 bg-gray-300">
+  //           <div className="flex flex-col gap-6 pb-20 justify-between">
+  //             <div className="flex flex-col gap-8 items-center text-center ">
+  //                <img
+  //                  src={Logo}
+  //                  alt="coart of arm logo"
+  //                  className="opacity-50 w-32 h-40 mx-auto"
+  //               />
+  //               <h1 className="text-lg font-bold">Lets verify who you are</h1>
+  //               <p>Enter the OTP received via your email</p>
+  //               <div className="flex items-center">
                
-                 <InputOTP maxLength={4}>
-                  <InputOTPGroup className="flex gap-4" >
-                    <InputOTPSlot 
-                      index={0} 
-                      style={{ 
-                        borderRadius: '8px',
-                        border: '2px solid black',
-                        height: '64px',
-                        width: '64px',
-                        fontSize: '32px',
-                      }}
-                    />
-                    <InputOTPSlot
-                      index={1} 
-                      style={{ 
-                        borderRadius: '8px',
-                        border: '2px solid black',
-                        height: '64px',
-                        width: '64px',
-                         fontSize: '32px'
-                      }}
-                    />
-                    <InputOTPSlot 
-                      index={2}  
-                      style={{ 
-                        borderRadius: '8px',
-                        border: '2px solid black',
-                        height: '64px',
-                        width: '64px',
-                        fontSize: '32px'
-                      }}
-                    />
-                    <InputOTPSlot 
-                      index={3} 
-                      style={{ 
-                        borderRadius: '8px',
-                        border: '2px solid black',
-                        height: '64px',
-                        width: '64px',
-                        fontSize: '32px'
-                      }}
+  //                <InputOTP maxLength={4}>
+  //                 <InputOTPGroup className="flex gap-4" >
+  //                   <InputOTPSlot 
+  //                     index={0} 
+  //                     style={{ 
+  //                       borderRadius: '8px',
+  //                       border: '2px solid black',
+  //                       height: '64px',
+  //                       width: '64px',
+  //                       fontSize: '32px',
+  //                     }}
+  //                   />
+  //                   <InputOTPSlot
+  //                     index={1} 
+  //                     style={{ 
+  //                       borderRadius: '8px',
+  //                       border: '2px solid black',
+  //                       height: '64px',
+  //                       width: '64px',
+  //                        fontSize: '32px'
+  //                     }}
+  //                   />
+  //                   <InputOTPSlot 
+  //                     index={2}  
+  //                     style={{ 
+  //                       borderRadius: '8px',
+  //                       border: '2px solid black',
+  //                       height: '64px',
+  //                       width: '64px',
+  //                       fontSize: '32px'
+  //                     }}
+  //                   />
+  //                   <InputOTPSlot 
+  //                     index={3} 
+  //                     style={{ 
+  //                       borderRadius: '8px',
+  //                       border: '2px solid black',
+  //                       height: '64px',
+  //                       width: '64px',
+  //                       fontSize: '32px'
+  //                     }}
 
-                     />
-                  </InputOTPGroup>
-                </InputOTP>
-                </div>
-              </div>
+  //                    />
+  //                 </InputOTPGroup>
+  //               </InputOTP>
+  //               </div>
+  //             </div>
               
-              <div className="flex items-center justify-center gap-8 mt-6 mb-8">
+  //             <div className="flex items-center justify-center gap-8 mt-6 mb-8">
+  //               <Button 
+  //                 onClick={moveToLoginPage}
+  //                 type="submit"
+  //                 className="h-16 w-64 rounded-full bg-orange-400 text-black hover:bg-orange-500 text-lg font-bold" >
+  //                 Verify
+  //               </Button>
+  //               {/* link to be implemented */}
+               
+  //             </div>
+  //           </div>
+  //         </form>
+        
+  //         <div className="md:flex items-center justify-center bg-muted p-8 h- bg-blue-600">
+  //           <img
+  //             src={home}
+  //             alt="e-passport"
+  //             className="max-w-full max-h-full object-contain" />
+  //         </div>
+  //       </CardContent>
+  //     </Card> 
+  //   </div>
+  // )
+      <form className={cn("flex flex-col gap-6 p-6 md:p-8 pb-12 bg-gray-200 rounded-xl", className)} {...props}>
+        
+          <div className="flex flex-col items-center gap-1 text-center">
+           <img
+            src={Logo}
+            alt="coart of arm logo"
+            className="opacity-50 w-40 h-40 mx-auto"
+          />
+            <h1 className="text-lg font-semibold">Lets verify who you are</h1>
+          </div>
+          
+          <div className="grid gap-2 mt-6 items-center justify-center">
+            <Label htmlFor="Username" className="font-bold text-lg">National ID</Label>
+              <Input className="rounded-xl border-opacity-30 border-black h-14 w-80 placeholder:text-lg placeholder:text-gray-500 text-lg"
+                id="nationalId" type="nationalId" 
+                placeholder="Enter National ID" 
+                required />
+           </div>
+            <div className="flex items-center justify-center gap-8 mt-20 mb-36">
                 <Button 
-                  onClick={moveToLoginPage}
+                  onClick={moveToOtpPage}
                   type="submit"
-                  className="h-16 w-64 rounded-full bg-orange-400 text-black hover:bg-orange-500 text-lg font-bold" >
+                  className="h-16 w-64 rounded-full bg-yellow-500 text-black hover:bg-yellow-600 text-xl font-bold" >
                   Verify
                 </Button>
-                {/* link to be implemented */}
-               
-              </div>
-            </div>
-          </form>
-        
-          <div className="md:flex items-center justify-center bg-muted p-8 h- bg-blue-600">
-            <img
-              src={home}
-              alt="e-passport"
-              className="max-w-full max-h-full object-contain" />
-          </div>
-        </CardContent>
-      </Card> 
-    </div>
+              {/* link to be implemented */}
+            </div> 
+      </form>
   )
 }
