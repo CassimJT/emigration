43f12d1393d7ff78d7cb3c1effc81479a6ef9cc2
bryan-nathupo <bryannathupo@gmail.com>
2026-01-31 09:48:19 +0200

    signup page integration implemented

--- src/features/auth/pages/SignupPage.jsx
+++ src/features/auth/pages/SignupPage.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { use, useEffect } from 'react'
 import SignupForm from '../components/SignupForm'
 import LoginAvatar from "@/assets/LoginAvatar.png"
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '@/features/auth/hooks/useAuth'
 
 function SignupPage() {
+  //form state
+  const [formState, setFormState] = React.useState({ email: '', password: '', confirmPassword: '' })
+  const navigate = useNavigate()
+  
+  const { 
+    signup,
+    error,  
+    loading,
+    status
+  } = useAuth()
+
+  //prepare signup payload
+  const preparePayload = () => {  
+    return {
+      emailAddress: formState.email.trim(),
+      password: formState.password,
+      confirmPassword: formState.confirmPassword,
+    }
+  }
+  //handling signup form field changes
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormState((prev) => ({ ...prev, [name]: value }))
+  }
+  //handling signup form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const payload = preparePayload()
+    await signup(payload)
+    
+  }
+  useEffect(() => {   
+    if (!status) return
+    if (status === 'success') {
+      navigate('/login', {replace: true})
+    }
+  }, [status,navigate])
+
   return (
     <div className="grid min-h-screen lg:grid-cols-2">
       <div className="flex flex-col items-center justify-center p-6 md:p-10 bg-white">
         <div className="w-full max-w-xl">
           <SignupForm />
+          values={formState}
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+          loading={loading}
+          error={error}
+         status={status}
         </div>
       </div>
       <div className="hidden lg:flex sticky top-0 h-screen items-center justify-center bg-green-100">
