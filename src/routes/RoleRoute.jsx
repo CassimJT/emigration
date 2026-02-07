34e570e04a627714e5bbe4aabee372a4a8d48a4e
lcfranklin <franklinlungu7@gmail.com>
2026-02-07 08:55:35 +0200

    admin dashboard init

--- src/routes/RoleRoute.jsx
+++ src/routes/RoleRoute.jsx
@@ -4,10 +4,10 @@ import { useAuth } from '../hooks/useAuth'
 export function RoleRoute({ allowedRoles }) {
   const { isAuthenticated, role } = useAuth()
 
-  // if (!isAuthenticated) return <Navigate to="/" replace />
-  // if (!allowedRoles.includes(role)) {
-  //   return <Navigate to="/unauthorized" replace />
-  // }
+  if (!isAuthenticated) return <Navigate to="/" replace />
+  if (!allowedRoles.includes(role)) {
+    return <Navigate to="/unauthorized" replace />
+  }
 
   return <Outlet />
 }
