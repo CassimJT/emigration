5cc2ed85fce2abf9f7ee19e3f6db0d6a13cd01bb
lcfranklin <franklinlungu7@gmail.com>
2026-02-12 20:57:56 +0200

    passport processing impl

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -82,7 +82,7 @@ export const router = createBrowserRouter([
           { path: 'passport/reviews', element: <PendingReviewsPage />, allowedRoles: ['officer','admin','superadmin'] },
           { path: 'stats', element: <StatisticsPage />, allowedRoles: ['officer','admin','superadmin'] },
           { path: 'users/me/profile', element: <ProfilePage />, allowedRoles: ['client','officer', 'admin','superadmin']},
-          { path:  'users', element: <ManageUsersPage />, allowedRoles: ['admin','superadmin']},
+          { path: 'admin/users', element: <ManageUsersPage />, allowedRoles: ['admin','superadmin']},
           { path: 'users/:userId', element: <UserDetailPage />, allowedRoles: ['admin','superadmin']},
           { path: 'passport/process/:applicationId', element: <PassportProcessingPage />, allowedRoles: ['admin','superadmin']},
         ]
